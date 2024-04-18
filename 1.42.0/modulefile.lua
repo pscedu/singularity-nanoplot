3d145768c72bea42cbaeb09bc05ba49e769450bd
icaoberg <icaoberg@andrew.cmu.edu>
2024-04-17 21:09:08 -0400

    Updated modulefile

--- 1.42.0/modulefile.lua
+++ 1.42.0/modulefile.lua
@@ -1,12 +1,12 @@
 --
--- nanoplot 1.42.0 modulefile
+-- NanoPlot 1.42.0 modulefile
 --
 -- "URL: https://www.psc.edu/resources/software"
 -- "Category: Biological Sciences"
 -- "Description: Plotting scripts for long read sequencing data."
 -- "Keywords: singularity bioinformatics"
 
-whatis("Name: nanoplot")
+whatis("Name: NanoPlot")
 whatis("Version: 1.42.0")
 whatis("Category: Biological Sciences")
 whatis("Keywords: singularity bioinformatics")
@@ -18,18 +18,18 @@ Plotting scripts for long read sequencing data.
 
 To load the module type
 
-> module load nanoplot/1.42.0
+> module load NanoPlot/1.42.0
 
 To unload the module type
 
-> module unload nanoplot/1.42.0
+> module unload NanoPlot/1.42.0
 
 Tools included in this module are
 
 * nanoplot
 ]])
 
-local package = "nanoplot"
+local package = "NanoPlot"
 local version = "1.42.0"
 local base    = pathJoin("/opt/packages",package,version)
 prepend_path("PATH", base)
