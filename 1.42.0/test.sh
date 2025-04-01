0d5e5118b442a2da1daae13ee8f25257df0a7d79
Ivan E. Cao-Berg <icaoberg@gmail.com>
2025-03-31 20:06:31 -0400

    Updated scripts

--- 1.42.0/test.sh
+++ 1.42.0/test.sh
@@ -8,25 +8,26 @@ else
     git clone https://github.com/wdecoster/nanotest.git
 fi
 
-./singularity-NanoPlot-1.42.0.sif -h
-./singularity-NanoPlot-1.42.0.sif --listcolors
+alias NanoPlot='./singularity-nanoplot-1.42.0.sif'
+
+NanoPlot --listcolors
 echo "testing figformat pdf with:"
-./singularity-NanoPlot-1.42.0.sif --bam nanotest/alignment.bam --verbose -o tests -o tests --format pdf --drop_outliers
+NanoPlot --bam nanotest/alignment.bam --verbose -o tests -o tests --format pdf --drop_outliers
 echo "testing fasta with --tsv_stats:"
-./singularity-NanoPlot-1.42.0.sif --fasta nanotest/reads.fa.gz --verbose --maxlength 35000 -o tests --tsv_stats
+NanoPlot --fasta nanotest/reads.fa.gz --verbose --maxlength 35000 -o tests --tsv_stats
 echo "testing bam:"
-./singularity-NanoPlot-1.42.0.sif --bam nanotest/alignment.bam --verbose -o tests --title testing
+NanoPlot --bam nanotest/alignment.bam --verbose -o tests --title testing
 echo "testing bam without supplementary alignments:"
-./singularity-NanoPlot-1.42.0.sif --bam nanotest/alignment.bam --verbose --no_supplementary -o tests
+NanoPlot --bam nanotest/alignment.bam --verbose --no_supplementary -o tests
 echo "testing summary:"
-./singularity-NanoPlot-1.42.0.sif --summary nanotest/sequencing_summary.txt --loglength --verbose -o tests
+NanoPlot --summary nanotest/sequencing_summary.txt --loglength --verbose -o tests
 echo "testing fastq rich:"
-./singularity-NanoPlot-1.42.0.sif --fastq_rich nanotest/reads.fastq.gz --verbose --downsample 800 -o tests
+NanoPlot --fastq_rich nanotest/reads.fastq.gz --verbose --downsample 800 -o tests
 echo "testing fastq minimal:"
-./singularity-NanoPlot-1.42.0.sif --fastq_minimal nanotest/reads.fastq.gz --store --verbose --plots dot -o tests
+NanoPlot --fastq_minimal nanotest/reads.fastq.gz --store --verbose --plots dot -o tests
 echo "testing fastq plain:"
-./singularity-NanoPlot-1.42.0.sif --fastq nanotest/reads.fastq.gz --verbose --minqual 4 --color red -o tests
+NanoPlot --fastq nanotest/reads.fastq.gz --verbose --minqual 4 --color red -o tests
 echo "testing fasta:"
-./singularity-NanoPlot-1.42.0.sif --fasta nanotest/reads.fa.gz --verbose --maxlength 35000 -o tests
+NanoPlot --fasta nanotest/reads.fa.gz --verbose --maxlength 35000 -o tests
 echo "testing --no_static:"
-./singularity-NanoPlot-1.42.0.sif --summary nanotest/sequencing_summary.txt  --verbose --maxlength 35000 -o tests --no_static
+NanoPlot --summary nanotest/sequencing_summary.txt  --verbose --maxlength 35000 -o tests --no_static
